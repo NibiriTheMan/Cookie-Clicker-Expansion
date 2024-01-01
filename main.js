3ef468b7717141465b197a12b53eeafbe88b4cb2
NibiriTheMan <154792176+NibiriTheMan@users.noreply.github.com>
2024-01-01 15:35:26 -0800

    Update main.js

--- main.js
+++ main.js
@@ -7027,9 +7027,6 @@ Game.Launch=function()
 					}
 					else
 					{	
-						if (Game.HaveAchiev('A round of applause').amount>1000) list.push(choose([
-						'News : deranged cookie manufacturer '+Game.bakeryName+' found to keep a collection of at least 1000 human hands!',
-						]));
 						if (Game.Objects['Farm'].amount>0) list.push(choose([
 						'News : cookie farms suspected of employing undeclared elderly workforce!',
 						'News : cookie farms release harmful chocolate in our rivers, says scientist!',
@@ -7229,6 +7226,7 @@ Game.Launch=function()
 				{
 					if (Math.random()<0.05)
 					{
+						if (Game.HasAchiev('A round of applause') list.push(choose(['News : deranged cookie manufacturer '+Game.bakeryName+' found to keep a collection of at least 1000 human hands!',]));
 						if (Game.HasAchiev('Base 10')) list.push('News : cookie manufacturer completely forgoes common sense, lets strange obsession with round numbers drive building decisions!');
 						if (Game.HasAchiev('From scratch')) list.push('News : follow the tear-jerking, riches-to-rags story about a local cookie manufacturer who decided to give it all up!');
 						if (Game.HasAchiev('A world filled with cookies')) list.push('News : known universe now jammed with cookies! No vacancies!');
@@ -8873,6 +8871,16 @@ Game.Launch=function()
 		Game.last.displayName='<span style="font-size:90%;letter-spacing:-1px;position:relative;bottom:2px;">Wizard tower</span>';//shrink
 		Game.last.minigameUrl='minigameGrimoire.js';
 		Game.last.minigameName=loc("Grimoire");
+		
+		new Game.Object('Shipment','shipment|shipments|shipped|[X] galaxy fully explored|[X] galaxies fully explored','Brings in fresh cookies from the cookie planet.',9,5,{base:'shipment',xV:16,yV:16,w:64,rows:1,x:0,y:0},40000,function(me){
+			var mult=1;
+			mult*=Game.GetTieredCpsMult(me);
+			mult*=Game.magicCpS(me.name);
+			return me.baseCps*mult;
+		},function(){
+			Game.UnlockTiered(this);
+			if (this.amount>=Game.SpecialGrandmaUnlock && Game.Objects['Grandma'].amount>0) Game.Unlock(this.grandma.name);
+		});		
 		
 		new Game.Object('Shipment','shipment|shipments|shipped|[X] galaxy fully explored|[X] galaxies fully explored','Brings in fresh cookies from the cookie planet.',9,5,{base:'shipment',xV:16,yV:16,w:64,rows:1,x:0,y:0},40000,function(me){
 			var mult=1;
