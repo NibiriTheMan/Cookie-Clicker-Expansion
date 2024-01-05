e3f2f65fb97e5c94a25bfca44217b4767da4b092
NibiriTheMan <154792176+NibiriTheMan@users.noreply.github.com>
2024-01-04 21:29:54 -0800

    Update main.js

--- main.js
+++ main.js
@@ -8904,7 +8904,8 @@ Game.Launch=function()
 			mult*=Game.magicCpS(me.name);
 			return me.baseCps*mult;
 		},function(){
-
+			Game.UnlockTiered(this);
+			if (this.amount>=Game.SpecialGrandmaUnlock && Game.Objects['Grandma'].amount>0) Game.Unlock(this.grandma.name);
 		});
 		
 		new Game.Object('Alchemy lab','alchemy lab|alchemy labs|transmuted|[X] primordial element mastered|[X] primordial elements mastered','Turns gold into cookies!',10,6,{base:'alchemylab',xV:16,yV:16,w:64,rows:2,x:0,y:16},200000,function(me){
