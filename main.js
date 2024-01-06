bdf5dd21793436db8b4ed2fa6e4931c6034300ba
NibiriTheMan <154792176+NibiriTheMan@users.noreply.github.com>
2024-01-06 00:03:49 -0800

    Update main.js

--- main.js
+++ main.js
@@ -12402,7 +12402,7 @@ Game.Launch=function()
 		Game.NewUpgradeCookie({name:'Illusium cookies',desc:'You\'re losing your touch...',icon:[22,37],power:						7,price: getCookiePrice(63)});
 		
 		order=590;
-		Game.TieredUpgrade('Workers','<q>Finally, now you can stop doing this stuff manually!','Sky citadel',1);
+		Game.TieredUpgrade('Workers','<q>Finally, now you can stop doing this stuff manually!','Sky citadel',2);
 		
 		
 
